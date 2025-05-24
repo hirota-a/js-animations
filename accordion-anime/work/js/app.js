226990acbf1d9726645157e2665aa854e2d808f2
hirota <aoaoworks766@gmail.com>
2025-05-25 00:56:00 +0900

    fix typo 4

--- accordion-anime/work/js/app.js
+++ accordion-anime/work/js/app.js
@@ -1,12 +1,20 @@
-const accordions = document.querySelectorAll('.list-item'); 
-accordions.forEach(accordion => { 
-    accordion.addEventListener('click', () => { 
-        const content = accordion.querySelector('.list-content'); 
-        content.classList.toggle('active'); 
+// すべての.list-titleを取得 クリックする部分
+const buttons = document.querySelectorAll(".list-title");
+
+// 各titleにクリックイベントを設定
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        // クリックされたボタンに紐づく要素(.list-content)を取得
+        const content = button.nextElementSibling;
+      
+        // activeクラスをトグル（あれば削除、なければ追加）
+        content.classList.toggle('active');
+        
         if(content.classList.contains('active')) { 
             content.style.height = content.scrollHeight + 'px'; 
-        } else { 
-            content.style.height = '0px'; 
-        } 
+        } else {
+            content.style.height = '0'; 
+        }
     });
 });
+
