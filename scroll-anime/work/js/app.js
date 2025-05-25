256961780739da01f63cd6350c48c004ca107686
hirota <aoaoworks766@gmail.com>
2025-05-25 09:55:42 +0900

    fix typo 5

--- scroll-anime/work/js/app.js
+++ scroll-anime/work/js/app.js
@@ -1,13 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {entry.target.classList.add("displayed");
-            } else {
-            entry.target.classList.remove("displayed");
-            }
-        });
-    },{threshold: 1,});
-    document.querySelectorAll(".box").forEach((box) => {
-        observer.observe(box);
-    });
+//HTMLを構成するDOMが形成された瞬間に発動する
+document.addEventListener('DOMContentLoaded', () => {
+	const scrollboxs = document.querySelectorAll('.box');//アニメーションさせたい要素のの取得
+
+	const options = {
+		threshold: 1,// 100%表示されたら反応 0.3= 30%表示されたら反応
+	};
+
+    //実行する処理
+	const callback = (entries => {
+	entries.forEach(entry => {
+		if(entry.isIntersecting) {
+			entry.target.classList.add('displayed');//entry.targetで対象の要素を取得できる
+        }else {
+            entry.target.classList.remove('displayed');//要素が画面外に出たらdisplayedクラスを削除
+        }
+		});
+	});
+
+    //Intersection Observer APIの設定
+	const observer = new IntersectionObserver(callback, options);
+
+	scrollboxs.forEach(target => {
+		observer.observe(target);
+	});
 });
